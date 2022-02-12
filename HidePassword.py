f700262cc063f7afa5d803b2b4daba95b1ff5fe6
Simon Atiegar <83236698+Simondelali@users.noreply.github.com>
2022-02-12 02:03:35 +0000

    initial commit

--- HidePassword.py
+++ HidePassword.py
@@ -2,3 +2,7 @@ from getpass import getpass
 print('hello world')
 username = input('Enter Username: ')
 password = getpass('Enter password: ')
+
+#Output :
+#Enter Username: simondelali
+#Enter password: ********  
