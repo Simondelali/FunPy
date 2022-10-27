f1014306b40a8719b5d1ad86af242713a5c26b61
Dela <83236698+Simondelali@users.noreply.github.com>
2022-10-27 22:19:32 +0000

    Add files via upload

---
+++ palindromes.py
@@ -0,0 +1,25 @@
+def is_palindrome(teststr):
+    #slice to reverse the string
+    if teststr == teststr[::-1]:
+        return True
+    else:
+        return False
+
+while True:
+    print("\n")
+    teststr = input("Enter Palindrome or 'exit':\n")
+    if teststr == 'exit':
+        break
+    
+    else:
+        #convert string to lower case
+        teststr = teststr.lower() 
+
+        newstr = ''
+        for x in teststr:
+            #check if character is alphanumeric and append
+            if x.isalnum():
+                newstr += x
+
+        print("Palindrome test -> ", is_palindrome(newstr))
+        
